d85de08385f24c9c21e22727d86acf56dc2fa821
Alex Salcedo <awsalcedo@sri.gob.ec>
2023-09-18 16:41:41 -0500

    Create frgaments layer ui and set colors

--- app/src/main/java/com/asalcedo/astroapp/ui/home/MainActivity.kt
+++ app/src/main/java/com/asalcedo/astroapp/ui/home/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.asalcedo.astroapp.ui
+package com.asalcedo.astroapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
---
+++ app/src/main/java/com/asalcedo/astroapp/ui/palmistry/PalMistryFragment.kt
@@ -0,0 +1,25 @@
+package com.asalcedo.astroapp.ui.palmistry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.asalcedo.astroapp.databinding.FragmentPalMistryBinding
+
+class PalMistryFragment : Fragment() {
+
+    private var _binding: FragmentPalMistryBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalMistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+}
