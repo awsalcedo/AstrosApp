6c34bfe06cf9a159d8168038e1ddfa0a22a18636
Alex Salcedo <awsalcedo@sri.gob.ec>
2023-10-04 16:21:10 -0500

    Add HoroscopeDetail page and set up safeargs in build.gradle.kts files

---
+++ app/src/main/java/com/asalcedo/astroapp/ui/detail/HoroscopeDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.asalcedo.astroapp.ui.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import com.asalcedo.astroapp.R
+import com.asalcedo.astroapp.databinding.ActivityHoroscopeDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HoroscopeDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHoroscopeDetailBinding
+    private val horoscopeDetailViewModel by viewModels<HoroscopeDetailViewModel>()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
