158cb517398f7a79885e1a769ad213d6fed3fd20
Alex Salcedo <awsalcedo@sri.gob.ec>
2023-10-23 16:17:52 -0500

    Add class to manage state logic to Horoscope Detail page

--- app/src/main/java/com/asalcedo/astroapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/asalcedo/astroapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -2,8 +2,13 @@ package com.asalcedo.astroapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor() : ViewModel() {
+    private var _state =
+        MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading) // initial state
+    val state: StateFlow<HoroscopeDetailState> = _state
 }
